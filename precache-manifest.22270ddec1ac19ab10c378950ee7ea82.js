7679c1b3e60ebecb2a9a6025a666a40b65ba8c10
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-06-23 17:21:23 -0600

    Updates

---
+++ precache-manifest.22270ddec1ac19ab10c378950ee7ea82.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fea86ae96f51b9f1d9cf00e804d01c9c",
+    "url": "/todolist-rest-api/index.html"
+  },
+  {
+    "revision": "71418f421b3c8cbcda2b",
+    "url": "/todolist-rest-api/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8c13006a926d8f0db4f7",
+    "url": "/todolist-rest-api/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "71418f421b3c8cbcda2b",
+    "url": "/todolist-rest-api/static/js/2.0559e60e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todolist-rest-api/static/js/2.0559e60e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c13006a926d8f0db4f7",
+    "url": "/todolist-rest-api/static/js/main.aa1eec9c.chunk.js"
+  },
+  {
+    "revision": "af3ac6a41baff9fa4a24",
+    "url": "/todolist-rest-api/static/js/runtime-main.756c23a5.js"
+  }
+]);
