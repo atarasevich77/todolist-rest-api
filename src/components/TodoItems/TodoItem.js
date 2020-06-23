7ac0919a9e19cf615df5e7ce732dfd93b79a29f7
kano-kenji <aleksandrtarasevich77@gmail.com>
2020-06-23 15:15:33 -0600

    Add buttons logo

--- src/components/TodoItems/TodoItem.js
+++ src/components/TodoItems/TodoItem.js
@@ -1,5 +1,16 @@
 import React, {useEffect, useRef, useState} from 'react';
 
+const cancelLogo = <svg className="bi bi-x-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+    <path fillRule="evenodd" d="M11.854 4.146a.5.5 0 0 1 0 .708l-7 7a.5.5 0 0 1-.708-.708l7-7a.5.5 0 0 1 .708 0z"/>
+    <path fillRule="evenodd" d="M4.146 4.146a.5.5 0 0 0 0 .708l7 7a.5.5 0 0 0 .708-.708l-7-7a.5.5 0 0 0-.708 0z"/>
+</svg>;
+
+const acceptLogo = <svg className="bi bi-check-square" width="1em" height="1em" viewBox="0 0 16 16" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+    <path fillRule="evenodd" d="M14 1H2a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+    <path fillRule="evenodd" d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.236.236 0 0 1 .02-.022z"/>
+</svg>;
+
 const TodoItem = (props) => {
     const nameNode = useRef(null);
     const descNode = useRef(null);
@@ -57,8 +68,8 @@ const TodoItem = (props) => {
                                onChange={(e) => setTodo({...todo, name: e.target.value})}
                         />
                         <div className="input-group-append">
-                            <button className="btn btn-outline-secondary" type="button">Button</button>
-                            <button className="btn btn-outline-secondary" type="button">Button</button>
+                            <span className="input-group-text">{cancelLogo}</span>
+                            <span className="input-group-text">{acceptLogo}</span>
                         </div>
                     </div>
                     :
@@ -78,8 +89,8 @@ const TodoItem = (props) => {
                                onChange={(e) => setTodo({...todo, description: e.target.value})}
                         />
                         <div className="input-group-append">
-                            <button className="btn btn-outline-secondary" type="button">Button</button>
-                            <button className="btn btn-outline-secondary" type="button">Button</button>
+                            <span className="input-group-text">{cancelLogo}</span>
+                            <span className="input-group-text">{acceptLogo}</span>
                         </div>
                     </div>
                     :
